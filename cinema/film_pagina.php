8b8c27d6833706c055d30a9e5400b5e1a0a7e6dd
Naeib17 <6036015@mborijnland.nl>
2024-12-06 22:57:11 +0100

    ik ben moe

---
+++ cinema/film_pagina.php
@@ -0,0 +1,13 @@
+<body>
+
+    <article class="films_art_bl">
+        <?php include 'assets/header_black.php'; ?>
+
+        <section class="film_mid">
+            <h1>naam van de film</h1>
+            <section>
+                <p>WE PROVIDE FULL-SERVICE POST-PRODUCTION WITH SEAMLESS COLLABORATION, SO YOU CAN MAINTAIN YOUR CREATIVE STANDARDS AND VISION ALL THE WAY THROUGH FINISHING.</p>
+            </section>
+        </section>
+    </article>
+</body>
