ce2574f18a3799ed61bb80c661a1abc57121cbdf
chahid1530 <147724226+chahid1530@users.noreply.github.com>
2024-12-06 19:07:53 +0100

    verbeteren
    verbeter

--- cinema/contact.php
+++ cinema/contact.php
@@ -1,1 +1,21 @@
 <?php include 'assets/header.php'; ?>
+
+<body>
+    <div class="container2">
+        <p>YOUR VISION. FAST. ON-TIME.<br>
+        DELIVERED TO YOUR CREATIVE STANDARDs.</p>
+        <h1>Cinema Studio</h1>
+        <form action="#" method="post" class="login-form2">
+            <label for="name">NAME:</label>
+            <input type="text" id="name" name="name" required>
+            
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="email">Tekst:</label>
+            <input type="email" id="email" name="email" required>
+            
+            <button type="submit" class="login-btn2">SEND</button>
+           
+        </form>
+       
