d5394f50d60e4648c6f422eaf37e565ed96e17b1
Naeib17 <6036015@mborijnland.nl>
2024-12-06 22:34:08 +0100

    film gemaakt

--- cinema/films.php
+++ cinema/films.php
@@ -1,1 +1,102 @@
 <?php include 'assets/header.php'; ?>
+
+<body>
+    <section class="funnel">
+    <i class="fa-solid fa-filter" style="color: #000000;"></i>
+    </section>
+
+    <article class="parent2">
+
+        <section>
+            <img src="img/home1.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home2.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home3.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home4.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home5.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home6.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home7.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home8.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home9.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home3.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home6.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home3.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home1.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home4.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+
+        <section>
+            <img src="img/home5.jpeg" alt="">
+            <h1>Movie name</h1>
+            <p>2024  .  102m</p>
+        </section>
+ 
+
+    </article>
+</body>
