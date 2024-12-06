8742de5d729f1b372555673bb1b0a1ea36ecb6ef
chahid1530 <147724226+chahid1530@users.noreply.github.com>
2024-12-06 11:01:51 +0100

    change
    Login

---
+++ cinema/login.php
@@ -0,0 +1,24 @@
+<?php include 'assets/header.php'; ?>
+
+
+
+<body>
+    <div class="container">
+        <h1>LOGIN</h1>
+        <form action="#" method="post" class="login-form">
+            <label for="name">NAME:</label>
+            <input type="text" id="name" name="name" required>
+            
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            
+            <button type="submit" class="login-btn">LOGIN</button>
+            <a href="#" class="signup-link">sign up</a>
+        </form>
+        <footer class="footer-text">
+            YOUR VISION. FAST. ON-TIME.<br>
+            DELIVERED TO YOUR CREATIVE STANDARDS.
+        </footer>
+    </div>
+</body>
+</html>
